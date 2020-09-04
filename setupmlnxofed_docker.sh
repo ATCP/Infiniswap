2cf37a0a9bbe2c523e04c2953033653b7d1ae4e6
ltgoter <ltfwimn@gmail.com>
2020-09-05 01:04:33 +0800

    addd

--- setupmlnxofed_docker.sh
+++ setupmlnxofed_docker.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -x
+
 cd /root
-wget https://www.mellanox.com/downloads/ofed/MLNX_OFED-3.3-1.0.4.0/MLNX_OFED_LINUX-3.3-1.0.4.0-ubuntu14.04-x86_64.tgz
-tar xvf MLNX_OFED_LINUX-3.3-1.0.4.0-ubuntu14.04-x86_64.tgz 
-cd MLNX_OFED_LINUX-3.3-1.0.4.0-ubuntu14.04-x86_64
-./mlnxofedinstall --force
+# wget https://www.mellanox.com/downloads/ofed/MLNX_OFED-3.3-1.0.4.0/MLNX_OFED_LINUX-3.3-1.0.4.0-ubuntu14.04-x86_64.tgz
+# tar xvf MLNX_OFED_LINUX-3.3-1.0.4.0-ubuntu14.04-x86_64.tgz 
+wget http://content.mellanox.com/ofed/MLNX_OFED-4.1-1.0.2.0/MLNX_OFED_LINUX-4.1-1.0.2.0-ubuntu14.04-x86_64.tgz
+
+tar xvf MLNX_OFED_LINUX-4.1-1.0.2.0-ubuntu14.04-x86_64.tgz
+cd MLNX_OFED_LINUX-4.1-1.0.2.0-ubuntu14.04-x86_64
+./mlnxofedinstall --force  --without-fw-update
 
 
 # install docker
@@ -27,13 +31,16 @@ stable"
 apt-get update
 apt-get install -y docker-ce=17.06.0~ce-0~ubuntu
 
-chmod +x *.sh */*.sh infiniswap_bd/nbdxadm/*
+cd /root/infiniswap
 
-#vim /etc/network/interfaces
+chmod +x *.sh */*.sh infiniswap_bd/nbdxadm/*
 
 
 # avoid host  lockup
 echo 1 > /proc/sys/kernel/softlockup_panic
+
+
+#vim /etc/network/interfaces
 
 # auto ib0
 # iface ib0 inet static
---
+++ updatekernel.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x
+
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.4-wily/linux-headers-4.4.0-040400_4.4.0-040400.201601101930_all.deb https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.4-wily/linux-headers-4.4.0-040400-generic_4.4.0-040400.201601101930_amd64.deb https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.4-wily/linux-image-4.4.0-040400-generic_4.4.0-040400.201601101930_amd64.deb
+
+dpkg -i *.deb
+
+update-grub

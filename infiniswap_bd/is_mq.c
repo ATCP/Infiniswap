8dd4d9efe8860aca138c1cd0ff96fa46a382ce6d
ltgoter <ltfwimn@gmail.com>
2020-09-14 15:47:14 +0800

    Update is_mq.c

--- infiniswap_bd/is_mq.c
+++ infiniswap_bd/is_mq.c
@@ -133,14 +133,14 @@ void stackbd_make_request5(struct bio *bio)
         goto abort;
     }
 
-	pr_info("lt:tag %d \n", 0);
+	// pr_info("lt:tag %d \n", 0);
     bio->bi_end_io = (bio_end_io_t*)IS_stackbd_end_io3;
 	pr_info("lt:tag %d, %p \n", 1, &stackbd.bio_list);
     bio_list_add(&stackbd.bio_list, bio);
 
     wake_up(&req_event);
     spin_unlock_irq(&stackbd.lock);
-	pr_info("lt:tag %d\n", 5);
+	// pr_info("lt:tag %d\n", 5);
     return;
 abort:
     spin_unlock_irq(&stackbd.lock);
